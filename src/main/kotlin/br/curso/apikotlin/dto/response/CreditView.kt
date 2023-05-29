8c4e134ccbd83844ddcf241fb07e42536a5db7ad
jribas-dev <3b3brazil@gmail.com>
2023-05-28 21:18:52 -0300

    Credit DTO

---
+++ src/main/kotlin/br/curso/apikotlin/dto/response/CreditView.kt
@@ -0,0 +1,24 @@
+package br.curso.apikotlin.dto.response
+
+import br.curso.apikotlin.entity.Credit
+import br.curso.apikotlin.enummeration.Status
+import java.math.BigDecimal
+import java.util.*
+
+data class CreditView(
+    val creditCode: UUID,
+    val creditValue: BigDecimal,
+    val numberOfInstallment: Int,
+    val status: Status,
+    val emailCustomer: String?,
+    val incomeCustomer: BigDecimal?
+) {
+    constructor(credit: Credit) : this(
+        creditCode = credit.creditCode,
+        creditValue = credit.creditValue,
+        numberOfInstallment = credit.numberOfInstallments,
+        status = credit.status,
+        emailCustomer = credit.customer?.email,
+        incomeCustomer = credit.customer?.income
+    )
+}
