86c87ea5c2eefd48ee6f0ab00c7d73aa6f48bf25
jribas-dev <3b3brazil@gmail.com>
2023-05-28 21:16:02 -0300

    Global exception handler

---
+++ src/main/kotlin/br/curso/apikotlin/exception/DetailsException.kt
@@ -0,0 +1,11 @@
+package br.curso.apikotlin.exception
+
+import java.time.LocalDateTime
+
+data class DetailsException(
+    val title: String,
+    val timestamp: LocalDateTime,
+    val status: Int,
+    val exception: String,
+    val details: MutableMap<String, String?>
+)
